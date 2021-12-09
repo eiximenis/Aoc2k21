79162b412d664c70e8ed4f8ded1ed9f9d1988459
eiximenis <etomas@gmail.com>
2021-12-09 19:30:18 +0100

    Quiz 3b

--- Aoc2k21.Tests/Quiz3Tests.cs
+++ Aoc2k21.Tests/Quiz3Tests.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using Aoc2k21.Quiz3;
+using FluentAssertions;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -14,11 +15,11 @@ namespace Aoc2k21.Tests
         [Fact]
         public async Task Given_Input_File_All_Entries_Should_Be_Returned()
         {
-            var data = new[] { "100110011100", "001100111101"};
+            var data = new[] { "100110011100", "001100111101" };
             var fname = Path.GetTempFileName();
             await File.WriteAllLinesAsync(fname, data);
 
-            var read = await Quiz3.GetInputData(fname);
+            var read = await Quiz3.Common.GetInputData(fname);
             var expected = data.Select(i => Convert.ToInt16(i, 2));
             read.Should().BeEquivalentTo(expected);
         }
@@ -29,7 +30,7 @@ namespace Aoc2k21.Tests
             var data = new[] { "00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001", "00010", "01010" };
             var input = data.Select(i => Convert.ToInt16(i, 2));
 
-            var (gamma, epsilon) = Quiz3.CalculateRates(input, 5);
+            var (gamma, epsilon) = Quiz3a.CalculateRates(input, 5);
             gamma.Should().Be(22);
             epsilon.Should().Be(9);
         }
@@ -37,10 +38,53 @@ namespace Aoc2k21.Tests
         [Fact]
         public void Given_All_Ones_Gamma_Should_Be_All_Ones_And_Epsilon_Should_Be_Zero()
         {
-            var data = new short [] { 0b111, 0b111, 0b111 };
-            var (gamma, epsilon) = Quiz3.CalculateRates(data, 3);
+            var data = new short[] { 0b111, 0b111, 0b111 };
+            var (gamma, epsilon) = Quiz3a.CalculateRates(data, 3);
             gamma.Should().Be(0b111);
             epsilon.Should().Be(0);
         }
     }
+
+    public class Quiz3bTests
+    {
+        [Fact]
+        public void Given_A_Single_Number_This_Number_Is_The_Oxygen_Generator_Rate()
+        {
+            var value = (short)0b10111;
+            var data = new[] { value };
+            var oxygen = Quiz3b.GetOxygenGeneratorRating(data, 5);
+            oxygen.Should().Be(value);
+        }
+
+        [Fact]
+        public void Given_Web_Inputs_Oxygen_Generator_Rate_Is_Correct()
+        {
+            var data = new[] { "00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001", "00010", "01010" };
+            var input = data.Select(i => Convert.ToInt16(i, 2));
+            var oxygen = Quiz3b.GetOxygenGeneratorRating(input, 5);
+            var expected = (short)23;
+            oxygen.Should().Be(expected);
+        }
+
+        [Fact]
+        public void Given_A_Single_Number_This_Number_Is_The_CO2_Scrubber_Rate()
+        {
+            var value = (short)0b10111;
+            var data = new[] { value };
+            var oxygen = Quiz3b.GetOxygenGeneratorRating(data, 5);
+            oxygen.Should().Be(value);
+        }
+
+        [Fact]
+        public void Given_Web_Inputs_CO2_Scrubber_Rate_Is_Correct()
+        {
+            var data = new[] { "00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001", "00010", "01010" };
+            var input = data.Select(i => Convert.ToInt16(i, 2));
+            var co2 = Quiz3b.GetCO2SrubberRating(input, 5);
+            var expected = (short)10;
+            co2.Should().Be(expected);
+        }
+
+    }
+
 }
--- Aoc2k21/Quiz3.cs
+++ Aoc2k21/Quiz3.cs
@@ -4,16 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Aoc2k21;
+namespace Aoc2k21.Quiz3;
 
-public static class Quiz3
+public static class Common
 {
-
+    public static bool BinaryDigitIsOne(int idx, short value, int numbits)
+    {
+        return (value & (1 << (numbits - idx))) != 0;
+    }
 
     public static async Task<IEnumerable<short>> GetInputData(string fname) =>
-    (await File.ReadAllLinesAsync(fname)).
+        (await File.ReadAllLinesAsync(fname)).
         Select(l => Convert.ToInt16(l, 2));
+}
 
+public static class Quiz3a
+{
 
     public static (int Gamma, int Epsilon) CalculateRates(IEnumerable<short> values, int numbits)
     {
@@ -24,7 +30,7 @@ public static class Quiz3
             var ones = 0;
             foreach (var value in values)
             {
-                ones = BinaryDigitIsOne(idx, value, numbits) ? ones + 1 : ones;
+                ones = Common.BinaryDigitIsOne(idx, value, numbits) ? ones + 1 : ones;
             }
             if (ones > entries / 2)
             {
@@ -34,9 +40,42 @@ public static class Quiz3
 
         return (gamma, (ushort)~gamma & ((1 << numbits) - 1));
     }
+}
 
-    private static bool BinaryDigitIsOne(int idx, short value, int numbits)
+public static class Quiz3b
+{
+    public static short GetOxygenGeneratorRating(IEnumerable<short> values, int numbits)
     {
-        return (value & (1 << (numbits - idx))) != 0;
+        var oxygen = (short)0;
+        var current = values;
+        for (var idx = 1; idx <= numbits; idx++)
+        {
+            var ones = current.Where(v => Common.BinaryDigitIsOne(idx, v, numbits)).ToArray();
+            var zeros = current.Where(v => !Common.BinaryDigitIsOne(idx, v, numbits)).ToArray();
+            if (ones.Count() >= zeros.Count())
+            {
+                oxygen |= (short)(1 << numbits - idx);
+                current = ones;
+            }
+            else
+            {
+                current = zeros;
+            }
+        }
+
+        return oxygen;
+    }
+
+    public static short GetCO2SrubberRating(IEnumerable<short> values, int numbits)
+    {
+        var current = values;
+        for (var idx = 1; idx <= numbits && current.Count() > 1; idx++)
+        {
+            var ones = current.Where(v => Common.BinaryDigitIsOne(idx, v, numbits)).ToArray();
+            var zeros = current.Where(v => !Common.BinaryDigitIsOne(idx, v, numbits)).ToArray();
+            current = ones.Count() < zeros.Count() ? ones : zeros;
+        }
+
+        return current.Single();
     }
 }
